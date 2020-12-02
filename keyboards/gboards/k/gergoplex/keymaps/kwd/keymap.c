c6975b37376fd055aed857cf567e1c59e7bf8832
s k <=>
2020-12-02 02:36:11 +0100

    first usable layout for me

--- keyboards/gboards/k/gergoplex/keymaps/kwd/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/kwd/keymap.c
@@ -11,15 +11,15 @@
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
-#define NUMB 2 // numbers/motion
+#define MOVE 2 // numbers/motion
 
 /* Combomap
  *
  * ,---------------------------------------.  ,---------------------------------------.
- * |       |      ESC      |       |       |  |       |      ESC      |      BSLH     |
+ * |       |      TAB      |       |       |  |       |       |      ESC      BSLH    |
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
- * |       |      BSPC    ENT      |       |  |      LES     COLN    GRT      |       | 
- * |-------+-------+-------+--LMB--+--RMB--+  |-------+-------+-------+-------+-------|
+ * |       |      BSPC    DEL      |       |  |       |       |      ENT      |       |
+ * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
  * |       |      MINS     |       |       |  |      QUO     UNDR     |       |       |
  * `-------+-------+-------+-------+-------'  `---------------------------------------'
  *           .---------+---------+---------.  .---------+---------+---------.
@@ -36,54 +36,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
  * |CTRL/A |   S   |   D   |   F   |   G   |  |   H   |   J   |   K   |   L   |CTRL/; | 
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
- * |SHFT/Z |   X   |   C   |   V   |   B   |  |   N   |   M   |   ,<  |  .>   |SHFT/? |
+ * |SHFT/Z |   X   |   C   |   V   |   B   |  |   N   |   M   |   ,<  |  .>   |SHFT//?;|
  * `-------+-------+-------+-------+-------'  `---------------------------------------'
  *           .---------+---------+---------.  .---------+---------+---------.   
- *           |         |   META  |SPC(SYM) |  |BSPC(NUM)|   ALT   |   TAB   |
+ *           | META/ESC|   META  |SPC(SYM) |  |BSPC(NUM)|   ALT   |   TAB   |
  *           '---------+---------+---------'  '---------+---------+---------' 
  */
 [BASE] = LAYOUT_gergoplex(
     KC_Q,  			   KC_W, KC_E, KC_R, KC_T,  	KC_Y, KC_U, KC_I,    KC_O, 	 KC_P, 
-    MT(MOD_LCTL, KC_A),KC_S, KC_D, KC_F, KC_G,  	KC_H, KC_J, KC_K,    KC_L, 	 MT(MOD_LCTL, KC_SCLN),
-    MT(MOD_RSFT, KC_Z),KC_X, KC_C, KC_V, KC_B,  	KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_RSFT, KC_SLSH),
+    MT(MOD_LCTL, KC_A),KC_S, KC_D, KC_F, KC_G,  	KC_H, KC_J, KC_K,    KC_L, 	 MT(MOD_RCTL, KC_SCLN),
+    MT(MOD_LSFT, KC_Z),KC_X, KC_C, KC_V, KC_B,  	KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_RSFT, KC_SLSH),
 
- 		MT(MOD_LGUI, KC_ESC), MT(MOD_LALT, KC_ENT), LT(SYMB, KC_SPC),			 // Left
-		LT(NUMB, KC_SPC), KC_LSFT, MT(MOD_RSFT, KC_TAB)							// Right
+ 		KC_NO,      KC_LGUI, LT(SYMB,KC_SPC),	    // Left
+		MO(MOVE)           , KC_LALT, KC_TAB		// Right
     ),
 /* Keymap 1: Symbols layer
  * ,---------------------------------------.  ,---------------------------------------.
- * |   !   |   @   |   {   |   }   |   ~   |  |   *   |   7   |   8   |   9   |       |
+ * |   !   |   @   |   {   |   }   |   ~   |  |   *   |   7   |   8   |   9   |   \   |
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
- * |CTRL/# |   $   |   (   |   )   |   |   |  |   +   |   4   |   5   |   6   | CTRL/ | 
+ * |CTRL/# |   $   |   (   |   )   |   |   |  |   +   |   4   |   5   |   6   | CTRL/'| 
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
- * |SHFT/% |   ^   |   [   |   ]   |   &   |  |   =   |   1   |   2   |   3   | SHFT/ |
+ * |SHFT/% |   ^   |   [   |   ]   |   &   |  |   -   |   1   |   2   |   3   | SHFT/=|
  * `-------+-------+-------+-------+-------'  `---------------------------------------'
  *           .---------+---------+---------.  .---------+---------+---------.   
  *  	     |         |         | PRESSED |  |         |    0    |         |
  *           '---------+---------+---------'  '---------+---------+---------' 
  */
 [SYMB] = LAYOUT_gergoplex(
-    KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,   KC_GRV,  KC_TILD, KC_TRNS, KC_TRNS, KC_BSLS,
-    KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_BTN2,   KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT,
-    KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_BTN1,   KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_MINS,
-                      CMB_TOG, KC_SCLN, KC_EQL,   KC_EQL,  KC_SCLN, KC_DEL
+    KC_EXLM,               KC_AT,   KC_LCBR, KC_RCBR, KC_TILD,   KC_ASTR, KC_7, KC_8, KC_9, KC_BSLS,
+    MT(MOD_LCTL,KC_HASH) , KC_DLR,  KC_LPRN, KC_PIPE, KC_PIPE,   KC_PLUS, KC_4, KC_5, KC_6, MT(MOD_RCTL,KC_QUOT),
+    MT(MOD_LSFT, KC_PERC), KC_CIRC, KC_LBRC, KC_RBRC, KC_AMPR,   KC_MINS, KC_1, KC_2, KC_3, MT(MOD_RSFT,KC_EQL),
+                                      KC_NO, KC_LGUI, KC_TRNS,   KC_NO,   KC_0, KC_NO
     ),
 /* Keymap 2: Pad/Function layer
  * ,---------------------------------------.  ,---------------------------------------.
- * |  F1   |  F2   |  F3   |  F4   |  F5   |  |       |  HOME |       |  END  |       |
+ * |  F1   |  F2   |  F3   |  F4   |  F5   |  |  HOME | PGDWN |  PGUP |  END  |       |
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
- * |  F6   |  F7   |  F8   |  F9   |  F10  |  |  LFT  |  DWN  |  UP   |  RGT  | VOLUP | 
+ * |CTRL/F6|  F7   |  F8   |  F9   |  F10  |  |  LFT  |  DWN  |   UP  |  RGT  | VOLUP | 
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
- * |  F11  |  F12  |       |       |       |  |  MLFT | MDWN  |  MUP  |  MRGT | VOLDN |
+ * |SFT/F11| F12   |       |       |       |  |  MLFT | MDWN  |  MUP  |  MRGT | VOLDN |
  * `-------+-------+-------+-------+-------'  `---------------------------------------'
  *           .---------+---------+---------.  .---------+---------+---------.   
- *  	     |   F11   |   F12   |	       |  | PRESSED |   PLY   |   SKP   |
+ *  	     |         |         |	       |  | PRESSED |         |         |
  *           '---------+---------+---------'  '---------+---------+---------' 
  */
-[NUMB] = LAYOUT_gergoplex(
-    KC_1,  KC_2,  KC_3,  KC_4,  KC_5,		 		KC_6,  	KC_7, 	 KC_8, 		KC_9, 	 KC_0,
-    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,   		KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
-    KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,  		KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD,
-     						  KC_F11,KC_F12,KC_TRNS,  	KC_TRNS,KC_MPLY,KC_MNXT
+[MOVE] = LAYOUT_gergoplex(
+    KC_F1,               KC_F2,  KC_F3, KC_F4, KC_F5,		KC_HOME, KC_PGDN, KC_PGUP, KC_END, 	KC_NO,
+    MT(MOD_LCTL,KC_F6),  KC_F7,  KC_F8, KC_F9, KC_F10,   	KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
+    MT(MOD_LSFT,KC_F11), KC_F12, KC_NO, KC_NO, KC_NO,  		KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD,
+            					 KC_NO, KC_NO, KC_NO,    	KC_TRNS, KC_LALT, KC_NO
     )
 };
