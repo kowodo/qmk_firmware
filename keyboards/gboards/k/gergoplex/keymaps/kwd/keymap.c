6f755d6bea767bc2f40d08a9cc6166a06922bd41
kowodo <stepan.krivanec@gmail.com>
2020-12-04 23:03:26 +0100

    Enter in another position, volume button removed from pinkies

--- keyboards/gboards/k/gergoplex/keymaps/kwd/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/kwd/keymap.c
@@ -95,9 +95,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------.  ,---------------------------------------.
  * |  F1   |  F2   |  F3   |  F4   |  F5   |  |  HOME | PGDWN |  PGUP |  END  |       |
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
- * |CTRL/F6|  F7   |  F8   |  F9   |  F10  |  |  LFT  |  DWN  |   UP  |  RGT  | VOLUP | 
+ * |CTRL/F6|  F7   |  F8   |  F9   |  F10  |  |  LFT  |  DWN  |   UP  |  RGT  | PGUP | 
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
- * |ALT/F11| F12   |       |       |       |  |  MLFT | MDWN  |  MUP  |  MRGT | VOLDN |
+ * |ALT/F11| F12   |       |       |       |  |  MLFT | MDWN  |  MUP  |  MRGT | PGDWN |
  * `-------+-------+-------+-------+-------'  `---------------------------------------'
  *           .---------+---------+---------.  .---------+---------+---------.   
  *  	     |         |         |	       |  | PRESSED |   ALT   |         |
@@ -105,8 +105,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [MOVE] = LAYOUT_gergoplex(
     KC_F1,               KC_F2,  KC_F3, KC_F4, KC_F5,		KC_HOME, KC_PGDN, KC_PGUP, KC_END, 	KC_NO,
-    MT(MOD_LCTL,KC_F6),  KC_F7,  KC_F8, KC_F9, KC_F10,   	KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
-    LALT_T(KC_F11),      KC_F12, KC_NO, KC_NO, KC_NO,  		KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD,
+    MT(MOD_LCTL,KC_F6),  KC_F7,  KC_F8, KC_F9, KC_F10,   	KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MT(MOD_RCTL,KC_PGUP),
+    LALT_T(KC_F11),      KC_F12, KC_NO, KC_NO, KC_NO,  		KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, LALT_T(KC_PGDN),
             				KC_NO, KC_TRNS, MO(MOUSE),    	KC_TRNS, KC_TRNS, KC_NO
     ),
 
