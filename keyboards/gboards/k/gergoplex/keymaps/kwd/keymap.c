150587ca2ff32f17ef7948756cbc56b7812ee7b2
kowodo <stepan.krivanec@gmail.com>
2020-12-06 01:06:27 +0100

    insert key now on the keyboard too! :D

--- keyboards/gboards/k/gergoplex/keymaps/kwd/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/kwd/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *           '---------+---------+---------'  '---------+---------+---------' 
  */
 [BASE] = LAYOUT_gergoplex(
-    KC_Q,  			    KC_W, KC_E, KC_R, KC_T,  	KC_Y, KC_U, KC_I,    KC_O, 	 KC_P, 
+    KC_Q,  			    KC_W, KC_E, KC_R, KC_T,     	KC_Y, KC_U, KC_I,    KC_O, 	 KC_P, 
     MT(MOD_LCTL, KC_A), KC_S, KC_D, KC_F, KC_G,  	KC_H, KC_J, KC_K,    KC_L, 	 MT(MOD_RCTL, KC_SCLN),
     LALT_T(KC_Z),       KC_X, KC_C, KC_V, KC_B,  	KC_N, KC_M, KC_COMM, KC_DOT, LALT_T(KC_SLSH),
 
@@ -87,25 +87,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_gergoplex(
     KC_AMPR,               KC_PIPE, KC_LCBR, KC_RCBR, KC_TILD,   KC_ASTR, KC_7, KC_8, KC_9, KC_BSLS,
     MT(MOD_LCTL, KC_CIRC), KC_DLR,  KC_LPRN, KC_RPRN, KC_EXLM,   KC_PLUS, KC_4, KC_5, KC_6, MT(MOD_RCTL,KC_EQL),
-    LALT_T(KC_F23),        KC_HASH, KC_LBRC, KC_RBRC, KC_AT,    KC_MINS, KC_1, KC_2, KC_3, LALT_T(KC_0),
+    LALT_T(KC_F23),        KC_HASH, KC_LBRC, KC_RBRC, KC_AT,     KC_MINS, KC_1, KC_2, KC_3, LALT_T(KC_0),
                                       KC_NO, KC_LGUI, KC_TRNS,   MO(MOUSE),   KC_TRNS, KC_NO
     ),
 
 /* Keymap 2: Pad/Function layer
  * ,---------------------------------------.  ,---------------------------------------.
- * |  F1   |  F2   |  F3   |  F4   |  F5   |  |  HOME | PGDWN |  PGUP |  END  |       |
+ * |  F1   |  F2   |  F3   |  F4   |  F5   |  |  HOME | PGDWN |  PGUP |  END  | PGUP  |
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
- * |CTRL/F6|  F7   |  F8   |  F9   |  F10  |  |  LFT  |  DWN  |   UP  |  RGT  | PGUP | 
+ * |CTRL/F6|  F7   |  F8   |  F9   |  F10  |  |  LFT  |  DWN  |   UP  |  RGT  |  INS  | 
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
  * |ALT/F11| F12   |       |       |       |  |  MLFT | MDWN  |  MUP  |  MRGT | PGDWN |
  * `-------+-------+-------+-------+-------'  `---------------------------------------'
  *           .---------+---------+---------.  .---------+---------+---------.   
- *  	     |         |         |	       |  | PRESSED |   ALT   |         |
+ *    	     |         |         |	       |  | PRESSED |   ALT   |         |
  *           '---------+---------+---------'  '---------+---------+---------' 
  */
 [MOVE] = LAYOUT_gergoplex(
-    KC_F1,               KC_F2,  KC_F3, KC_F4, KC_F5,		KC_HOME, KC_PGDN, KC_PGUP, KC_END, 	KC_NO,
-    MT(MOD_LCTL,KC_F6),  KC_F7,  KC_F8, KC_F9, KC_F10,   	KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MT(MOD_RCTL,KC_PGUP),
+    KC_F1,               KC_F2,  KC_F3, KC_F4, KC_F5,		  KC_HOME, KC_PGDN, KC_PGUP, KC_END, 	KC_PGUP,
+    MT(MOD_LCTL,KC_F6),  KC_F7,  KC_F8, KC_F9, KC_F10,   	KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MT(MOD_RCTL,KC_INS),
     LALT_T(KC_F11),      KC_F12, KC_NO, KC_NO, KC_NO,  		KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, LALT_T(KC_PGDN),
             				KC_NO, KC_TRNS, MO(MOUSE),    	KC_TRNS, KC_TRNS, KC_NO
     ),
@@ -119,13 +119,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |       |       |       |       |       |  |       |       |       |       |       |
  * `-------+-------+-------+-------+-------'  `---------------------------------------'
  *           .---------+---------+---------.  .---------+---------+---------.   
- *  	     |         |         |	       |  | PRESSED |   ALT   |         |
+ *    	     |         |         |	       |  | PRESSED |   ALT   |         |
  *           '---------+---------+---------'  '---------+---------+---------' 
  */
 [MOUSE] = LAYOUT_gergoplex(
-    KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,		KC_NO,   KC_NO,   KC_NO,   KC_NO, 	KC_NO,
+    KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,		        KC_NO,   KC_NO,   KC_NO,   KC_NO, 	KC_NO,
     KC_NO,  KC_ACL0, KC_ACL1, KC_ACL2, KC_NO,   	KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,  	KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD,
-            				   KC_NO, KC_TRNS, KC_TRNS,    	KC_TRNS, KC_TRNS, KC_NO
+    KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,  	        KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD,
+            				   KC_NO, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_NO
     )
 };
